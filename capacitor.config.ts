3660d4e69c68169f8eb9d1861ff68e3c275a4d93
jhonatact <jhonatatiene118@gmail.com>
2025-06-16 17:40:29 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projeto_pokemon',
+  webDir: 'www'
+};
+
+export default config;
