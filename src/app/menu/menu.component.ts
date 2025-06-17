56dee31e1f530678c604700cf75907210d6afc16
jhonatact <jhonatatiene118@gmail.com>
2025-06-17 10:34:07 -0300

    Componente menu
    1 - Criei um componente de menu
    
    2 - Importei as dependências
    
    3 - Adicionei o botão na tela principal

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  IonContent,
+  IonHeader,
+  IonMenu,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
+  imports: [IonContent, IonHeader, IonMenu, IonTitle, IonToolbar]
 })
-export class MenuComponent  implements OnInit {
+export class MenuComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
--- src/app/tela-principal/tela-principal.ts
+++ src/app/tela-principal/tela-principal.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonMenuButton } from '@ionic/angular/standalone';
+import { MenuComponent } from "../menu/menu.component";
 
 @Component({
   selector: 'app-tela-principal',
   templateUrl: 'tela-principal.html',
   styleUrls: ['tela-principal.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, MenuComponent, IonMenuButton],
 })
 export class TelaPrincipal {
   constructor() {}
