7a16190a03d6b4ea3d164e1f230aa1399eafd12b
jhonatact <jhonatatiene118@gmail.com>
2025-06-17 11:30:24 -0300

    routerLink nos botões
    1- Adicionei o routerLink nos botões Tela inicial e Favoritos
    
    2 - importei o RouterModule no MenuComponent para funcionar o routerLink

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  IonContent,
-  IonHeader,
-  IonMenu,
-  IonTitle,
-  IonToolbar, IonItem, IonList, IonLabel, IonIcon } from "@ionic/angular/standalone";
+import { IonContent, IonHeader, IonMenu, IonTitle, IonToolbar, IonItem, IonList, IonLabel, IonIcon } from "@ionic/angular/standalone";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
-  imports: [IonIcon, IonLabel, IonList, IonItem, IonContent, IonHeader, IonMenu, IonTitle, IonToolbar]
+  imports: [IonIcon, IonLabel, IonList, IonItem, IonContent, IonHeader, IonMenu, IonTitle, IonToolbar, RouterModule]
 })
 export class MenuComponent implements OnInit {
 
