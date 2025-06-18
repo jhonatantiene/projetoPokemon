95f5ad5824e39dd77068beca5ffcc04153e1fd2c
jhonatact <jhonatatiene118@gmail.com>
2025-06-18 10:05:38 -0300

    Chamada do  metodo buscarPokePorIdOuNome() e tratamento de erro
    1 - Adicionei o angular material no projeto
    
    2 - Fiz a chamada da função pokePorIdOuNome()
    
    3 - Implementei o MatSnackBar para exibir mensagem de erro ao falhar a requisição

--- src/app/tela-principal/tela-principal.ts
+++ src/app/tela-principal/tela-principal.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonMenuButton } from '@ionic/angular/standalone';
 import { MenuComponent } from "../menu/menu.component";
+import { PokeApiService } from '../servicos/poke-api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-tela-principal',
@@ -8,6 +10,26 @@ import { MenuComponent } from "../menu/menu.component";
   styleUrls: ['tela-principal.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, MenuComponent, IonMenuButton],
 })
-export class TelaPrincipal {
-  constructor() {}
+export class TelaPrincipal implements OnInit {
+  constructor(private api: PokeApiService, private snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.buscarPokePorIdOuNome('')
+  }
+
+  buscarPokePorIdOuNome(idOuNome: string) {
+    this.api.pokePorIdOuNome(idOuNome).subscribe({
+      next: (res) => {
+        console.log(res)
+      },
+      error: (err) => {
+        console.error(err.message)
+        this.snackBar.open('Erro ao buscar pokemon!', 'Fechar', {
+          duration: 5000
+        })
+      }
+    });
+  }
+
+
 }
