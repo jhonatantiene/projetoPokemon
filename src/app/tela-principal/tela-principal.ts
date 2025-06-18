926a698a209d970a92c0d1b0f80809194bee412e
jhonatact <jhonatatiene118@gmail.com>
2025-06-18 13:01:26 -0300

    Implementando logica das funções buscarPokePorIdOuNome() e buscarTodosPoke()
    1- Implementei o metodo buscarTodosPoke() para listar todos os pokemons com imagem oficial
    
    2 - adicionei uma verificação no metodo buscarPokePorIdOuNome() caso o input seja vazio
    
    3 - removi os console.log() desnecessarios

--- src/app/tela-principal/tela-principal.ts
+++ src/app/tela-principal/tela-principal.ts
@@ -13,18 +13,47 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class TelaPrincipal implements OnInit {
   constructor(private api: PokeApiService, private snackBar: MatSnackBar) { }
 
+  listaPokemons: any = []
+
   ngOnInit(): void {
-    this.buscarPokePorIdOuNome('')
+    this.buscarTodosPoke()
   }
 
-  buscarPokePorIdOuNome(idOuNome: string) {
-    this.api.pokePorIdOuNome(idOuNome).subscribe({
-      next: (res) => {
-        console.log(res)
+  buscarTodosPoke() {
+    this.api.listarTodosPoke().subscribe({
+      next: (res: any) => {
+        let id = undefined
+        this.listaPokemons = res.results.map((v: any) => {
+          id = v.url.split('/').slice(6, 7)[0] //pega o id da imagem
+          return {
+            nome: v.name,
+            imgOficial: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+          }
+        })
       },
       error: (err) => {
         console.error(err.message)
         this.snackBar.open('Erro ao buscar pokemon!', 'Fechar', {
+          duration: 5000
+        })
+      }
+    })
+  }
+
+  buscarPokePorIdOuNome(idOuNome: string) {
+    if (idOuNome.trim() == '' || idOuNome == undefined || idOuNome == null) {
+      return
+    }
+    this.api.pokePorIdOuNome(idOuNome).subscribe({
+      next: (res: any) => {
+        this.listaPokemons = [{
+          nome: res.name,
+          imgOficial: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${res.id}.png`
+        }]
+      },
+      error: (err) => {
+        console.error(err.message)
+        this.snackBar.open('Pokemon não encontrado!', 'Fechar', {
           duration: 5000
         })
       }
