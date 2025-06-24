5ea531dad7fdf98b6624398af18a3e3fd1d2478a
jhonatact <jhonatatiene118@gmail.com>
2025-06-24 17:58:02 -0300

    função de adicionar e remover favoritos
    1 - crie as funções de adicionar e remover um item favorito do localStorage
    
    2 - adicionei a estrutura no html

--- src/app/tela-detalhes/tela-detalhes.component.ts
+++ src/app/tela-detalhes/tela-detalhes.component.ts
@@ -3,6 +3,7 @@ import { IonHeader, IonToolbar, IonMenuButton, IonContent, IonCard } from "@ioni
 import { PokeApiService } from '../servicos/poke-api.service';
 import { ActivatedRoute } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-tela-detalhes',
@@ -13,32 +14,94 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 })
 export class TelaDetalhesComponent implements OnInit {
 
-  constructor(private rota: ActivatedRoute, private api: PokeApiService) { }
+  constructor(private rota: ActivatedRoute, private api: PokeApiService, private snackBar: MatSnackBar) { }
 
   imagens: any
   informacoes: any
+  jaExisteNosFavoritos: boolean = true
 
-  ngOnInit() {
-    const id = this.rota.snapshot.paramMap.get('id')
-    if (id) {
-      this.api.pokePorIdOuNome(id).subscribe((res: any) => {
-        this.imagens = [
-          { titulo: 'Oficial', url: res.sprites.other?.['official-artwork']?.front_default },
-          { titulo: 'Oficial_shiny', url: res.sprites.other?.['official-artwork']?.front_shiny },
-        ].filter(img => img.url)
-
-        this.informacoes = {
-          nome: res.name,
-          tipos: res.types.map((t: any) => t.type.name),
-          altura: res.height / 10,
-          peso: res.weight / 10,
-          habilidades: res.abilities.map((h: any) => h.ability.name),
-          id: res.id
-        }
-      })
-    }
-
+  async ngOnInit() {
+    await this.infoPokeSelecionado()
+    this.alteraBotaoFavorito()
   }
 
+  infoPokeSelecionado(): Promise<void> {
+    return new Promise((resolve) => {
+      const id = this.rota.snapshot.paramMap.get('id')
+
+      if (id) {
+        this.api.pokePorIdOuNome(id).subscribe({
+          next: (res: any) => {
+            this.imagens = [
+              { titulo: 'Oficial', url: res.sprites.other?.['official-artwork']?.front_default },
+              { titulo: 'Oficial_shiny', url: res.sprites.other?.['official-artwork']?.front_shiny },
+            ].filter(img => img.url)
+
+            this.informacoes = {
+              nome: res.name,
+              tipos: res.types.map((t: any) => t.type.name),
+              altura: res.height / 10,
+              peso: res.weight / 10,
+              habilidades: res.abilities.map((h: any) => h.ability.name),
+              id: res.id,
+              urlImg: res.sprites.other?.['official-artwork']?.front_default
+            }
+
+            resolve()
+          }
+        })
+      } else {
+        return
+      }
+    })
+  }
+
+  alteraBotaoFavorito(): Promise<void> {
+    return new Promise((resolve) => {
+      const favoritosSalvos = localStorage.getItem('favoritos')
+      const favoritos = favoritosSalvos ? JSON.parse(favoritosSalvos) : []
+      this.jaExisteNosFavoritos = favoritos.some((p: any) => p.id === this.informacoes!.id)
+      resolve()
+    })
+  }
+
+  adicionarAosFavoritos() {
+    if (!this.informacoes) {
+      return
+    }
+    const favoritosSalvos = localStorage.getItem('favoritos')
+    const favoritos = favoritosSalvos ? JSON.parse(favoritosSalvos) : []
+    const jaExiste = favoritos.some((p: any) => p.id === this.informacoes!.id)
+
+    if (!jaExiste) {
+      favoritos.push(this.informacoes)
+      localStorage.setItem('favoritos', JSON.stringify(favoritos))
+    } else {
+      return
+    }
+
+    this.alteraBotaoFavorito()
+    this.snackBar.open('Adicionado aos favoritos', '', {
+      duration: 1000
+    })
+  }
+
+  removerDosFavoritos() {
+    if (!this.informacoes) return;
+
+    const favoritosSalvos = localStorage.getItem('favoritos')
+    const favoritos = favoritosSalvos ? JSON.parse(favoritosSalvos) : []
+
+    const novosFavoritos = favoritos.filter((p: any) => p.id !== this.informacoes.id)
+
+    localStorage.setItem('favoritos', JSON.stringify(novosFavoritos))
+    this.jaExisteNosFavoritos = false
+
+    this.alteraBotaoFavorito()
+
+    this.snackBar.open('Removido dos favoritos', '', {
+      duration: 1000
+    })
+  }
 
 }
