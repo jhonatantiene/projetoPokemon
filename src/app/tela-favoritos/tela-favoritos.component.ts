0dc972b5e2a62d43f5e5bdc157262d0b7cae9874
jhonatact <jhonatatiene118@gmail.com>
2025-06-24 18:01:19 -0300

    Tela de favoritos
    1 - criei as funções para buscar os pokemons favoritos no localStorage e a função ver detalhes que redireciona para tela de detalhes
    
    2 - criei a estrutura no html
    
    3- implementei o design no scss

--- src/app/tela-favoritos/tela-favoritos.component.ts
+++ src/app/tela-favoritos/tela-favoritos.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonMenuButton, IonContent, IonList, IonItem, IonItemSliding, IonThumbnail, IonLabel, IonIcon, IonButtons } from "@ionic/angular/standalone";
+import { ViewWillEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-tela-favoritos',
   templateUrl: './tela-favoritos.component.html',
   styleUrls: ['./tela-favoritos.component.scss'],
+  imports: [IonButtons, IonIcon, IonLabel, IonItemSliding, IonItem, IonList, IonContent, IonToolbar, IonHeader, IonMenuButton, IonThumbnail]
 })
-export class TelaFavoritosComponent  implements OnInit {
+export class TelaFavoritosComponent implements ViewWillEnter {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
-  ngOnInit() {}
+  listaFavoritos: any = []
+
+  ionViewWillEnter() {
+    this.buscarFavoritos()
+  }
+
+  buscarFavoritos() {
+    this.listaFavoritos = localStorage.getItem('favoritos')
+    this.listaFavoritos = JSON.parse(this.listaFavoritos)
+  }
+
+  verDetalhes(id: number) {
+    this.route.navigate(['/telaDetalhes', id])
+  }
 
 }
